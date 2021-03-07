65cc9a95a9932da5a4adb7c3822974485c43bc63
Oliver Green <green2go@gmail.com>
2021-03-07 09:11:47 +0000

    Missed fixes.

--- src/Api/Enums/VehicleLookupFlags.php
+++ src/Api/Enums/VehicleLookupFlags.php
@@ -9,38 +9,32 @@ class VehicleLookupFlags extends AbstractEnum
         // Provides most recent MOT test information for the specified vehicle.
         MOT_TESTS = 'motTests';
     const
-        
-        
+
         // VehicleLookupFlags::FEATURES
         // Provides an array of standard and possible optional features for the specified vehicle.
         FEATURES = 'features';
     const
-        
-        
+
         // VehicleLookupFlags::BASIC_VEHICLE_CHECK
         // Provides a variety of vehicle specific provenance data.
         BASIC_VEHICLE_CHECK = 'basicVehicleCheck';
     const
-        
-        
+
         // VehicleLookupFlags::FULL_VEHICLE_CHECK
         // Provides a variety of vehicle specific provenance data.
         FULL_VEHICLE_CHECK = 'fullVehicleCheck';
     const
-        
-        
+
         // VehicleLookupFlags::VALUATIONS
         // Provides a variety of Auto Trader valuations for the specified vehicle.
         VALUATIONS = 'valuations';
     const
-        
-        
+
         // VehicleLookupFlags::VEHICLE_METRICS
         // Provides a variety of Auto Trader valuations and vehicle metrics for the specified vehicle.
         VEHICLE_METRICS = 'vehicleMetrics';
     const
-        
-        
+
         // VehicleLookupFlags::COMPETITORS
         // Provides a pre-constructed URL, allowing users to explore market competition.
         COMPETITORS = 'competitors';
