7dc2662730149c182bc5f4fd3194fff3501e2124
Oliver Green <green2go@gmail.com>
2021-03-08 09:52:27 +0000

    Added the ability whitelist attributes from the array output filter.

--- src/Api/Builders/AbstractBuilder.php
+++ src/Api/Builders/AbstractBuilder.php
@@ -13,6 +13,13 @@ abstract class AbstractBuilder
     protected $requiredAttributes = [];
 
     /**
+     * Attributes that are allowed to be empty.
+     *
+     * @var array
+     */
+    protected $allowEmpty = [];
+
+    /**
      * AbstractBuilder constructor.
      *
      * @param array $attributes
@@ -22,11 +29,29 @@ abstract class AbstractBuilder
         $this->setAttributes($attributes);
     }
 
+    /**
+     * Get a friendly human readable name for the class.
+     *
+     * @return string
+     */
     public function getFriendlyName(): string
     {
         $class = new \ReflectionClass($this);
 
         return $class->getShortName();
+    }
+
+    /**
+     * Add attribute to the 'empty' whitelist.
+     *
+     * @param string $key
+     * @return $this
+     */
+    public function allowEmpty(string $key): AbstractBuilder
+    {
+        $this->allowEmpty[] = $key;
+
+        return $this;
     }
 
     /**
@@ -93,12 +118,25 @@ abstract class AbstractBuilder
         return $this;
     }
 
+    /**
+     * Checks whether a given key is that of a child builder.
+     *
+     * @param string $key
+     * @return bool
+     */
     private function attributeIsBuilder(string $key): bool
     {
         return property_exists($this, $key) &&
             $this->$key instanceof AbstractBuilder;
     }
 
+    /**
+     * Set an attributes value.
+     *
+     * @param string $key
+     * @param $value
+     * @return $this
+     */
     public function setAttribute(string $key, $value): AbstractBuilder
     {
         // Set values that have defined setters.
@@ -118,23 +156,34 @@ abstract class AbstractBuilder
      *
      * @param array $output
      *
-     * @return array]
+     * @return array
      */
-    protected function filterPrepareOutput(array $output)
+    protected function filterPrepareOutput(array $output): array
     {
-        /*return array_filter($output, function($item) {
-            return !is_null($item);
-        });*/
-        return array_filter($output, function ($item) {
-            return isset($item) && !(is_array($item) && empty($item));
-        });
+        return array_filter($output, function ($item, $key) {
+            return in_array($key, $this->allowEmpty) ||
+                isset($item) && !(is_array($item) && empty($item));
+        }, ARRAY_FILTER_USE_BOTH);
     }
 
+    /**
+     * Get the attributes required to be
+     * set for validation.
+     *
+     * @return string[]
+     */
     public function getRequiredAttributes(): array
     {
         return $this->requiredAttributes;
     }
 
+    /**
+     * Set the list of attributes required to
+     * be set fo validation.
+     *
+     * @param array $keys
+     * @return $this
+     */
     public function setRequiredAttributes(array $keys): AbstractBuilder
     {
         $this->requiredAttributes = $keys;
@@ -142,29 +191,63 @@ abstract class AbstractBuilder
         return $this;
     }
 
+    /**
+     * Checks whether an attribute key is empty;
+     *
+     * @param $key
+     * @return bool
+     */
     protected function attributeEmpty($key): bool
     {
-        return empty($key);
+        return empty($this->$key);
     }
 
+    /**
+     * Validates the state of the builder.
+     *
+     * @return bool
+     * @throws ValidationException
+     */
     public function validate(): bool
     {
         foreach ($this->requiredAttributes as $key) {
-            if ($this->attributeEmpty($this->$key)) {
-                throw new ValidationException($this->getFriendlyName().": $key must not be empty.");
+            if ($this->attributeEmpty($key)) {
+                throw new ValidationException(
+                    $this->getFriendlyName().": $key must not be empty."
+                );
             }
         }
 
         return true;
     }
 
+    /**
+     * Gets the array representation of the builder.
+     *
+     * @return array
+     */
     abstract public function toArray(): array;
 
+    /**
+     * Gets the JSON formatted representation of the builder.
+     *
+     * @param null $options
+     * @return false|string
+     */
     public function toJson($options = null)
     {
         return json_encode($this->toArray(), $options);
     }
 
+    /**
+     * Gets an item from an array returning the
+     * default if it does not exist.
+     * 
+     * @param array $array
+     * @param $key
+     * @param false $default
+     * @return false|mixed
+     */
     protected function dataGet(array $array, $key, $default = false)
     {
         if (array_key_exists($key, $array)) {
