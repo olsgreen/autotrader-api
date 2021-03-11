bd09c324526c85b6c2ecb4374b116c8d98be326b
Oliver Green <green2go@gmail.com>
2021-03-11 12:18:19 +0000

    Added service history enum.

---
+++ src/Api/Enums/ServiceHistory.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Olsgreen\AutoTrader\Api\Enums;
+
+
+class ServiceHistory
+{
+    const FULL_SERVICE_HISTORY = 'Full service history';
+
+    const FULL_DEALERSHIP_HISTORY = 'Full dealership history';
+
+    const SERVICE_HISTORY = 'Service history';
+
+    const PART_SERVICE_HISTORY = 'Part service history';
+}
