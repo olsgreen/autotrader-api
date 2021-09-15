d48dc2be47d6543475f82a70c3a934c40fadd1a2
Oliver Green <green2go@gmail.com>
2021-09-15 11:49:36 +0200

    Basic retail metrics implementation.

--- src/Api/Builders/MetricRequestBuilder.php
+++ src/Api/Builders/MetricRequestBuilder.php
@@ -2,6 +2,38 @@
 
 namespace Olsgreen\AutoTrader\Api\Builders;
 
-class MetricRequestBuilder
+class MetricRequestBuilder extends ValuationRequestBuilder
 {
+    protected $location;
+
+    public function setLocation(array $latLng): self
+    {
+        if (empty($latLng['latitude']) || empty($latLng['longitude'])) {
+            throw new \Exception('Location array missing either latitude or longitude');
+        }
+
+        $this->location = [
+            'latitude' => $latLng['latitude'],
+            'longitude' => $latLng['longitude']
+        ];
+
+        return $this;
+    }
+
+    public function getLocation():? array
+    {
+        return $this->location;
+    }
+
+    public function toArray(): array
+    {
+        $array = parent::toArray();
+
+        return $this->filterPrepareOutput(
+            array_merge(
+                $array,
+                ["location" => $this->location]
+            )
+        );
+    }
 }
--- src/Api/VehicleMetrics.php
+++ src/Api/VehicleMetrics.php
@@ -6,7 +6,22 @@ use Olsgreen\AutoTrader\Api\Builders\MetricRequestBuilder;
 
 class VehicleMetrics extends AbstractApi
 {
-    public function lookup(MetricRequestBuilder $metricsRequest)
+    /**     *
+     * $request = MetricRequestBuilder::create();.
+     *
+     * $request->vehicle()
+     *  ->setDerivativeId('ABC123')
+     *  ->setFirstRegisteredDate('2020-11-01')
+     *  ->setOdometerReadingMiles(8000);
+     *
+     * $valuation = $api->vehicleMmetrics()->retrieve($request)
+     */
+    public function lookup(string $advertiserId, MetricRequestBuilder $builder)
     {
+        return $this->_post(
+            '/service/stock-management/vehicleMetrics',
+            ['advertiseId' => $advertiserId],
+            $builder->toJson()
+        );
     }
 }
