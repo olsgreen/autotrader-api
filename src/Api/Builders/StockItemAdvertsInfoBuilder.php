9ffdfe67b8f80bf726bb45a3a40300e069af8fd1
Oliver Green <qwerty@hey.com>
2023-05-03 22:51:55 +0100

    Allow omission of 'reservationStatus' key.

--- src/Api/Builders/StockItemAdvertsInfoBuilder.php
+++ src/Api/Builders/StockItemAdvertsInfoBuilder.php
@@ -6,7 +6,7 @@ use Olsgreen\AutoTrader\Api\Enums\ReservationStatuses;
 
 class StockItemAdvertsInfoBuilder extends AbstractBuilder
 {
-    protected $allowEmpty = ['reservationStatus'];
+    protected $allowEmpty = [];
 
     protected $forecourtPrice;
 
