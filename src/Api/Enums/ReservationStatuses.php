85de57b805b934f89a1ff022766ca83c7b2f61c6
Oliver Green <qwerty@hey.com>
2024-06-07 11:13:41 +0100

    Reservation Statuses: Update reservation status inline with A/T API updates.

--- src/Api/Enums/ReservationStatuses.php
+++ src/Api/Enums/ReservationStatuses.php
@@ -4,9 +4,5 @@ namespace Olsgreen\AutoTrader\Api\Enums;
 
 class ReservationStatuses extends AbstractEnum
 {
-    const RESERVED_BY_ADVERTISER = 'Reserved By Advertiser';
-
-    const RESERVED_BY_AUTOTRADER = 'Reserved By Auto Trader';
-
-    const RESERVATION_BY_AUTOTRADER_CLAIMED = 'Reservation By Auto Trader Claimed';
+    const RESERVED = 'Reserved';
 }
